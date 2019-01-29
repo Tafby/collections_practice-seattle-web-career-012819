e8d71ce04f422ed5ffe3be4bb9d792eb4a409cf4
Tiffany Carter <tafby88@gmail.com>
2019-01-29 01:28:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,6 +8,6 @@ end
 def sort_array_char_count(string_array)
   string_array.sort { |a,b| a.length <=> b.length }
 end
-def swap_elements
+def swap_elements(array)
   array[1], array[2] = array[2], array[1]
 end

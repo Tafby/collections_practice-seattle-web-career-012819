7107d1572acca73093ab76c6219a3f7b117e2612
Tiffany Carter <tafby88@gmail.com>
2019-01-29 01:25:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,5 +6,5 @@ def sort_array_desc(int_array)
   int_array.sort.reverse
 end
 def sort_array_char_count(string_array)
-  string_array.sort { |a,b| a.length <=> b.length
+  string_array.sort { |a,b| a.length <=> b.length }
 end
